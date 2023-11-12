508b80297069ff2252694d2b04975e1434aad779
gisetaboada1990 <giseltaboada1990@gmail.com>
2023-11-12 01:25:29 -0300

    deploy

--- src/db.js
+++ src/db.js
@@ -29,7 +29,7 @@ let sequelize =
         ssl: true,
       })
     : new Sequelize(
-        `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/pokemon`,
+        `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/country`,
         { logging: false, native: false }
       );
 const basename = path.basename(__filename);
